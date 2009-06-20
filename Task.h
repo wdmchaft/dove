6805de190fcaec88918c7fc568fb0d001c87d995
zuwiki <zuwiki@gmail.com>
2009-06-19 20:37:32 -0700

    More doc

--- Task.h
+++ Task.h
@@ -12,5 +12,9 @@
 }
 @property (retain) NSString *action;
 @property (retain) NSDate *dueDate;
+// Both NSNumber and BOOL fail here. BOOL won't even compile (saying "property
+// 'completed' with 'retain' attribute must be of object type"), and NSNumber
+// produces the runtime error shown in DVModel.m
+// I am pretty sure my main question is what type should completed be?
 @property (retain) NSNumber *completed;
 @end
